6d3439464bd10c21f39c31b9d3a69b42049f064e
Петър Стоев <petarstoev0505@gmail.com>
2023-03-26 21:43:12 +0300

    v1

---
+++ QuizApp/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Banner from "./components/Banner.js";
+import Header from "./components/Header.js";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      
+    </div>
+  );
+}
+
+export default App;
